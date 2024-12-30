03a3676f7759f0ff3d3e3ef4093bf3b99431c7ef
Albert Luppol <albert.y.luppol@gmail.com>
2024-12-29 21:43:21 -0800

    TextExtractor: full update

--- src/TextClassifier.py
+++ src/TextClassifier.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from sklearn.preprocessing import MinMaxScaler
 import json
 
-from src.Text_Extractor_Outdated import FeaturedText
+from TextExtractor import FeaturedText
 from LabelTransformer import LabelTransformer
 from LabelTransformer import Label
 
