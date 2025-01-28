e8c50271c0cb8875d2211a8aa092ba47b897f517
Albert Luppol <albert.y.luppol@gmail.com>
2025-01-27 23:18:20 -0800

    TextAnalyzer: init draft

--- src/TextAnalyzer.py
+++ src/TextAnalyzer.py
@@ -1,15 +1,51 @@
 from typing import List
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from pathlib import Path
 
 
 class TextAnalyzer:
-    def __init__(self):
-        pass
+    def __init__(self, model_dir='src/models/img_to_speech-book_text_analyzer_model', model_name='meta-llama/Llama-2-7b-chat-hf'):
+        try:
+            self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+            self.model = AutoModelForCausalLM.from_pretrained(model_name=model_name)
+            print(f'Model ... loaded from {model_dir}')
+        except Exception as e:
+            print(f'Failed to load model from {model_dir} ({e}); Initializing model ... from {model_name}')
+            self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+            self.model = AutoModelForCausalLM.from_pretrained(model_name=model_name)
+
+            path_to_model_dir = Path(model_dir)
+            if not path_to_model_dir.exists():
+                Path(model_dir).mkdir(parents=True, exist_ok=True)
+
+            self.tokenizer.save_pretrained(model_dir)
+            self.model.save_pretraied(model_dir)
+            
 
     def is_the_same_paragraph(self, end_of_page: str, beginning_of_next_page: str) -> bool:
-        pass
+        prompt = f'''Does sentences in the tripple brackets belong to the same paragraph? \n
+            """ {end_of_page}"""\n"""{beginning_of_next_page}"""\n
+            Answer with "yes" or "no" only.'''
+        inputs = self.tokenizer(prompt, return_tensors='pt')
+        outputs = self.model.generate(**inputs, max_length=10)
+        answer = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return 'yes' in answer.lower()
 
     def split_text_to_paragraphs(self, text: str) -> List[str]:
-        pass
+        prompt = f'''Split text provided in the tripple brackets into paragraphs: \n
+            """ {text}"""\n
+            Provide answer in the next form:
+            Paragraph 1\n\nParagraph 2\n\nParagraph3\n\n...
+            \n\n\nNo metter what not include anything else other then exactly the same text that was provided splitted into described format'''
+        inputs = self.tokenizer(prompt, return_tensors='pt')
+        outputs = self.model.generate(**inputs, max_length=1000)
+        paragraphs = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return paragraphs.split("\n\n")
 
-    def match_annotations_to_the_text(self, text: str, annotations: List[str]) -> List[str]:
-        pass
+    def is_annotation_belong_to_the_text(self, text: str, annotation: str) -> bool:
+        prompt = f'''Does annotation provided in the tripple brackets belong to the text provided in double brackets? \n
+            """ {annotation}"""\n""{text}""\n
+            Answer with "yes" or "no" only.'''
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        outputs = self.model.generate(**inputs, max_length=10)
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
